8c8ddf78b6cbc79470c8aa51aec0511cc0918176
ahmads1990 <ahmads20002000@gmail.com>
2024-03-21 09:37:14 +0200

    Multiple comp

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import "./App.css";
+import Home from "./Home";
+import Navbar from "./Navbar";
 
 function App() {
+	const title = "Welcome to the new blog";
+	const likes = 50;
+
 	return (
 		<div className="App">
+			<Navbar />
+
 			<div className="content">
-				<h1>App comps</h1>
+				<Home />
 			</div>
 		</div>
 	);
