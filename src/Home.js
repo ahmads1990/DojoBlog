e3090f7d34f745b486d9ecdde8a08c4840b146e4
ahmads1990 <ahmads20002000@gmail.com>
2024-03-24 10:42:27 +0200

    UseEffect

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
@@ -12,6 +12,10 @@ const Home = () => {
 		const newBlogs = blogs.filter((b) => b.id !== id);
 		setBlogs(newBlogs);
 	};
+
+	useEffect(() => {
+		console.log("useEffect", blogs);
+	}, []);
 
 	return (
 		<div className="home">
