808abc940c8cd7ca2bd3648d8a462153d9e4bf50
Ahmad <ahmads20002000@gmail.com>
2024-07-22 07:56:16 +0300

    Update api urls using env variables

--- src/Home.js
+++ src/Home.js
@@ -2,16 +2,18 @@ import BlogList from "./BlogList";
 import useFetch from "./useFetch";
 
 const Home = () => {
-	const { data: blogs, isPending, error } = useFetch("http://localhost:8000/blogs");
+    const apiUrl = process.env.REACT_APP_API_URL;
+    console.log(apiUrl);
+    const { data: blogs, isPending, error } = useFetch(apiUrl + "blog");
 
-	return (
-		<div className="home">
-			{error && <div>{error}</div>}
-			{/* All blogs */}
-			{isPending && <div>Loading...</div>}
-			{blogs && <BlogList blogs={blogs} title="All blogs!" />}
-		</div>
-	);
+    return (
+        <div className="home">
+            {error && <div>{error}</div>}
+            {/* All blogs */}
+            {isPending && <div>Loading...</div>}
+            {blogs && <BlogList blogs={blogs} title="All blogs!" />}
+        </div>
+    );
 };
 
 export default Home;
