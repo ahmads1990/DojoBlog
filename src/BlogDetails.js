23a3eef7f12d660098c92f11bb0158ac14ee1846
Ahmad <ahmads20002000@gmail.com>
2024-07-25 07:26:12 +0300

    Update blog details and createBlog to use new api

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -3,32 +3,33 @@ import useFetch from "./useFetch";
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
 const BlogDetails = () => {
-	const { id } = useParams();
-	const { data: blog, error, isPending } = useFetch("http://localhost:8000/blogs/" + id);
-	const history = useHistory();
+    const { id } = useParams();
+    const apiUrl = process.env.REACT_APP_API_URL;
+    const { data: blog, error, isPending } = useFetch(apiUrl + "blog/" + id);
+    const history = useHistory();
 
-	const handleClick = () => {
-		fetch("http://localhost:8000/blogs/" + blog.id, {
-			method: "DELETE",
-		}).then(() => {
-			history.push("/");
-		});
-	};
+    const handleClick = () => {
+        fetch(apiUrl + "blog/?id=" + blog.id, {
+            method: "DELETE",
+        }).then(() => {
+            history.push("/");
+        });
+    };
 
-	return (
-		<div className="blog-details">
-			{isPending && <div>Loading...</div>}
-			{error && <div>{error}</div>}
-			{blog && (
-				<article>
-					<h2>{blog.title}</h2>
-					<p>Written by {blog.author}</p>
-					<div>{blog.body}</div>
-					<button onClick={handleClick}>delete</button>
-				</article>
-			)}
-		</div>
-	);
+    return (
+        <div className="blog-details">
+            {isPending && <div>Loading...</div>}
+            {error && <div>{error}</div>}
+            {blog && (
+                <article>
+                    <h2>{blog.title}</h2>
+                    <p>Written by {blog.author.name}</p>
+                    <div>{blog.description}</div>
+                    <button onClick={handleClick}>delete</button>
+                </article>
+            )}
+        </div>
+    );
 };
 
 export default BlogDetails;
--- src/Create.js
+++ src/Create.js
@@ -1,52 +1,61 @@
 import { useState } from "react";
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+import useFetch from "./useFetch";
 
 const Create = () => {
-	const [title, setTitle] = useState("");
-	const [body, setBody] = useState("");
-	const [author, setAuthor] = useState("mario");
-	const [isPending, setIsPending] = useState(false);
-	const history = useHistory();
+    const [title, setTitle] = useState("");
+    const [body, setBody] = useState("");
+    const [author, setAuthor] = useState(0);
+    const [isPending, setIsPending] = useState(false);
+    const history = useHistory();
+    const apiUrl = process.env.REACT_APP_API_URL;
+    const { data: authors, isPending: authorsPending, error: authorsError } = useFetch(apiUrl + "author");
 
-	const handleSubmit = (e) => {
-		e.preventDefault();
-		const blog = { title, body, author };
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const blog = { title, description: body, AuthorId: author };
 
-		setIsPending(true);
+        setIsPending(true);
 
-		fetch("http://localhost:8000/blogs", {
-			method: "POST",
-			headers: { "Content-Type": "application/json" },
-			body: JSON.stringify(blog),
-		}).then(() => {
-			console.log("new blog added");
-			setIsPending(false);
-			// history.go(-1);
-			history.push("/");
-		});
-	};
+        fetch(apiUrl + "blog", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(blog),
+        }).then(() => {
+            console.log("new blog added");
+            setIsPending(false);
+            // history.go(-1);
+            history.push("/");
+        });
+    };
 
-	return (
-		<div className="create">
-			<h2>Add a New blog</h2>
-			<form onSubmit={handleSubmit}>
-				<label>Blog title:</label>
-				<input type="text" required value={title} onChange={(e) => setTitle(e.target.value)} />
+    return (
+        <div className="create">
+            <h2>Add a New blog</h2>
+            <form onSubmit={handleSubmit}>
+                <label>Blog title:</label>
+                <input type="text" required value={title} onChange={(e) => setTitle(e.target.value)} />
 
-				<label>Blog body</label>
-				<textarea required value={body} onChange={(e) => setBody(e.target.value)}></textarea>
+                <label>Blog body</label>
+                <textarea required value={body} onChange={(e) => setBody(e.target.value)}></textarea>
 
-				<label>Blog author:</label>
-				<select value={author} onChange={(e) => setAuthor(e.target.value)}>
-					<option value="mario">mario</option>
-					<option value="yoshi">yoshi</option>
-				</select>
+                <label>Blog author:</label>
+                {authorsPending && <div>Loading authors...</div>}
+                {authorsError && <div>{authorsError}</div>}
+                <select value={author} onChange={(e) => setAuthor(e.target.value)} disabled={authorsPending}>
+                    {authors &&
+                        authors.map((author) => (
+                            <option key={author.id} value={author.id}>
+                                {author.name}
+                            </option>
+                        ))}
+                </select>
 
-				{!isPending && <button>Add blog</button>}
-				{isPending && <button disabled>Adding blog...</button>}
-			</form>
-		</div>
-	);
+                {!isPending && <button>Add blog</button>}
+                {isPending && <button disabled>Adding blog...</button>}
+            </form>
+        </div>
+    );
 };
 
 export default Create;
